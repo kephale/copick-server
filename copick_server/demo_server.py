2be4068e2c57cb9c23f44804ab977e5c47a7ed31
Kyle Harrington <czi@kyleharrington.com>
2025-04-24 11:44:12 -0400

    Update demo_server.py to use dataset_ids instead of config file

--- copick_server/demo_server.py
+++ copick_server/demo_server.py
@@ -1,7 +1,15 @@
-# As a module
+# Demo server using the dataset_ids parameter
 from copick_server.server import serve_copick
 
-serve_copick("/Users/kharrington/git/copick/copick-server/example_copick.json", allowed_origins=["*"])
+# Use dataset_id instead of config file
+serve_copick(dataset_ids=[14268], overlay_root="/tmp/overlay_root", allowed_origins=["*"])
 
-# Or from command line
-# $ python server.py path/to/copick_config.json --cors "*"
+# Previous config-based approach:
+# serve_copick("/Users/kharrington/git/copick/copick-server/example_copick.json", allowed_origins=["*"])
+
+# Command line usage:
+# With config file:
+# $ python -m copick_server.server serve --config path/to/copick_config.json --cors "*"
+# 
+# With dataset IDs:
+# $ python -m copick_server.server serve --dataset-ids 14268 --cors "*" --port 8017
