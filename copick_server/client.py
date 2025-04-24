46215a042b5ed62229d076b0448c672c50086f8a
Kyle Harrington <czi@kyleharrington.com>
2025-04-24 11:54:17 -0400

    Update demo for data portal and MLC

--- copick_server/client.py
+++ copick_server/client.py
@@ -4,13 +4,13 @@ import numpy as np
 from fsspec import get_mapper
 
 # First read the tomogram to get the shape
-store = get_mapper("http://localhost:8000/14268/Tomograms/VoxelSpacing7.84/wbp.zarr")
+store = get_mapper("http://localhost:8017/16463/Tomograms/VoxelSpacing10.012/wbp.zarr")
 tomo = zarr.open(store, mode='r')
 full_shape = tomo["0"].shape
 print(f"Tomogram shape: {full_shape}")
 
 # Example parameters
-voxel_size = 7.84  # must match your tomogram's voxel spacing
+voxel_size = 10.012
 user_id = "kisharrington"
 session_id = "copickServer"
 name = "membrane"  # This must match a pickable object in your config
@@ -33,7 +33,7 @@ shape_header = np.array(data.shape, dtype=np.int64).tobytes()
 data_bytes = shape_header + data.tobytes()
 
 # Send to server as a single request
-url = f"http://localhost:8000/14268/Segmentations/{seg_filename}"
+url = f"http://localhost:8017/16463/Segmentations/{seg_filename}"
 response = requests.put(url, data=data_bytes)
 
 if response.status_code == 200:
